e4312fd031c452c9825070b7e0fa3e947b6dcb92
Davide Mauri <damauri@microsoft.com>
2020-02-06 12:05:05 -0800

    fixed shell deployment script

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -3,23 +3,34 @@
 set -euo pipefail
 
 # Make sure these values are correct for your environment
-resourceGroup="azure-sql-db-python-rest-api"
-appName="azure-sql-db-python-rest-api"
+resourceGroup="dm-api-01"
+appName="dm-api-01"
 location="WestUS2" 
 
 # Change this if you are using your own github repository
 gitSource="https://github.com/Azure-Samples/azure-sql-db-python-rest-api.git"
 
+# Make sure connection string variable is set
+
+if [[ -z "${SQLAZURECONNSTR_WWIF:-}" ]]; then
+	echo "Plase export Azure SQL connection string:";
+    echo "export SQLAZURECONNSTR_WWIF=\"your-connection-string-here\"";
+	exit 1;
+fi
+
+echo "Creating Resource Group...";
 az group create \
     -n $resourceGroup \
     -l $location
 
+echo "Creating Application Service Plan...";
 az appservice plan create \
     -g $resourceGroup \
     -n "linux-plan" \
     --sku B1 \
     --is-linux
 
+echo "Creating Web Application...";
 az webapp create \
     -g $resourceGroup \
     -n $appName \
@@ -28,8 +39,9 @@ az webapp create \
     --deployment-source-url $gitSource \
     --deployment-source-branch master
 
+echo "Configuring Connection String...";
 az webapp config connection-string set \
     -g $resourceGroup \
     -n $appName \
-    --settings WWIF=$SQLAZURECONNSTR_WWIF \
+    --settings WWIF="$SQLAZURECONNSTR_WWIF" \
     --connection-string-type=SQLAzure
